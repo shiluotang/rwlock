0ae83d8dd226c624cee25cd47fd004975cb09361
Quangang Sheng <quangang.sheng@wz-inc.com>
2016-07-14 20:32:26 +0800

    rename vendor macros

--- src/main.c
+++ src/main.c
@@ -7,9 +7,22 @@
 
 #define DUMP_MACRO(X) printf(#X " = 0x%lX\n", (long) (X))
 
+#ifdef _WIN32
+#   define _WIN32_DEFINED "yes"
+#else
+#   define _WIN32_DEFINED "no"
+#endif
+#ifdef _WIN64
+#   define _WIN64_DEFINED "yes"
+#else
+#   define _WIN64_DEFINED "no"
+#endif
+
 int main(int argc, char* *argv) {
     DUMP_MACRO(WINVER);
     DUMP_MACRO(_WIN32_WINNT);
     printf("RWLock vendor is %s\n", RWLockVendor());
+    printf("_WIN32 is defined: %s\n", _WIN32_DEFINED);
+    printf("_WIN64 is defined: %s\n", _WIN64_DEFINED);
     return EXIT_SUCCESS;
 }
--- src/rwlock.c
+++ src/rwlock.c
@@ -12,36 +12,36 @@ static char const* const RWLOCK_VENDORS[] = {
     "Windows Software Simulation"
 };
 
-#define PTHREAD_RWLOCK_VENDOR 0
-#define WINDOWS_SRW_RWLOCK_VENDOR 1
-#define WINDOWS_SIM_RWLOCK_VENDOR 2
+#define RWLOCK_VENDOR_PTHREAD 0
+#define RWLOCK_VENDOR_WINDOWS_SRW 1
+#define RWLOCK_VENDOR_WINDOWS_SIM 2
 
-#ifdef HAVE_PTHREAD_H
+#if defined(HAVE_PTHREAD_H) && defined(HAVE_LIBPTHREAD)
 #   include <pthread.h>
-#   define RWLOCK_VENDOR PTHREAD_RWLOCK_VENDOR
+#   define RWLOCK_VENDOR RWLOCK_VENDOR_PTHREAD
 #elif defined(_MSC_VER)
 #   include <windows.h>
 #   if  (defined(WINVER) && WINVER >= 0x601) || (defined(_WIN32_WINNT) && _WIN32_WINNT >= 0x601)
-#       define RWLOCK_VENDOR WINDOWS_SRW_RWLOCK_VENDOR
+#       define RWLOCK_VENDOR RWLOCK_VENDOR_WINDOWS_SRW
 #       if (defined(WINVER) && WINVER > 0x601) || (defined(_WIN32_WINNT) && _WIN32_WINNT > 0x601)
 #           include <synchapi.h>
 #       endif
 #   else
-#       define RWLOCK_VENDOR WINDOWS_SIM_RWLOCK_VENDOR
+#       define RWLOCK_VENDOR RWLOCK_VENDOR_WINDOWS_SIM
 #   endif
 #elif defined(__CYGWIN__) || defined(__MINGW32__) || defined(__MINGW64__)
-#   define RWLOCK_VENDOR WINDOWS_SIM_RWLOCK_VENDOR
+#   define RWLOCK_VENDOR RWLOCK_VENDOR_WINDOWS_SIM
 #   include <windows.h>
 #else
 #   error Can not determine which platform this is.
 #endif
 
 struct tagRWLock {
-#if RWLOCK_VENDOR == WINDOWS_SRW_RWLOCK_VENDOR
+#if RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SRW
 	SRWLOCK _M_locker;
-#elif RWLOCK_VENDOR == PTHREAD_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_PTHREAD
 	pthread_rwlock_t _M_locker;
-#elif RWLOCK_VENDOR == WINDOWS_SIM_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SIM
 #endif
 };
 
@@ -50,14 +50,14 @@ RWLock* RWLockCreate() {
 	lock = (RWLock*) malloc(sizeof(RWLock));
 	if (!lock)
 		return lock;
-#if RWLOCK_VENDOR == WINDOWS_SRW_RWLOCK_VENDOR
+#if RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SRW
 	InitializeSRWLock(&lock->_M_locker);
-#elif RWLOCK_VENDOR == PTHREAD_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_PTHREAD
 	if (pthread_rwlock_init(&lock->_M_locker, NULL) == 0)
 		return lock;
 	free(lock);
 	lock = NULL;
-#elif RWLOCK_VENDOR == WINDOWS_SIM_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SIM
 	// TODO
 #endif
     return lock;
@@ -72,56 +72,56 @@ void RWLockDestroy(RWLock *lock) {
 }
 
 int RWLockReadLock(RWLock *lock) {
-#if RWLOCK_VENDOR == WINDOWS_SRW_RWLOCK_VENDOR
+#if RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SRW
 	AcquireSRWLockShared(&lock->_M_locker);
 	return 1;
-#elif RWLOCK_VENDOR == PTHREAD_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_PTHREAD
 	if (pthread_rwlock_rdlock(&lock->_M_locker) == 0)
 		return 1;
 	return 0;
-#elif RWLOCK_VENDOR == WINDOWS_SIM_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SIM
 	// TODO
 	return 0;
 #endif
 }
 
 int RWLockReadUnlock(RWLock *lock) {
-#if RWLOCK_VENDOR == WINDOWS_SRW_RWLOCK_VENDOR
+#if RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SRW
 	ReleaseSRWLockShared(&lock->_M_locker);
 	return 1;
-#elif RWLOCK_VENDOR == PTHREAD_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_PTHREAD
 	if (pthread_rwlock_unlock(&lock->_M_locker) == 0)
 		return 1;
 	return 0;
-#elif RWLOCK_VENDOR == WINDOWS_SIM_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SIM
 	// TODO
 	return 0;
 #endif
 }
 
 int RWLockWriteLock(RWLock *lock) {
-#if RWLOCK_VENDOR == WINDOWS_SRW_RWLOCK_VENDOR
+#if RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SRW
 	AcquireSRWLockExclusive(&lock->_M_locker);
 	return 1;
-#elif RWLOCK_VENDOR == PTHREAD_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_PTHREAD
 	if (pthread_rwlock_wrlock(&lock->_M_locker) == 0)
 		return 1;
 	return 0;
-#elif RWLOCK_VENDOR == WINDOWS_SIM_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SIM
 	// TODO
 	return 0;
 #endif
 }
 
 int RWLockWriteUnlock(RWLock *lock) {
-#if RWLOCK_VENDOR == WINDOWS_SRW_RWLOCK_VENDOR
+#if RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SRW
 	ReleaseSRWLockExclusive(&lock->_M_locker);
 	return 1;
-#elif RWLOCK_VENDOR == PTHREAD_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_PTHREAD
 	if (pthread_rwlock_unlock(&lock->_M_locker) == 0)
 		return 1;
 	return 0;
-#elif RWLOCK_VENDOR == WINDOWS_SIM_RWLOCK_VENDOR
+#elif RWLOCK_VENDOR == RWLOCK_VENDOR_WINDOWS_SIM
 	// TODO
 	return 0;
 #endif
